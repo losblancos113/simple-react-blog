5651e1cfe576585d4d815c8376e3eb38ba277806
Tri Hoang Minh <tri.hoangminh@nttdata.com>
2016-05-16 14:09:47 +0700

    Finish course

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,16 +9,36 @@ class PostsIndex extends Component{
         this.props.fetchPosts();
     }
 
+    renderPosts(){
+        return this.props.posts.map((post) => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={'posts/' + post.id}>
+                        <span className="pull-xs-right">{post.categories}</span>
+                        <strong>{post.title}</strong>
+                    </Link>
+                </li>
+            );
+        });
+    }
+
     render(){
         return (
             <div>
                 <div className="text-xs-right">
                     <Link to="posts/new" className="btn btn-primary">Add a post</Link>
                 </div>
-                List posts
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default connect(null, {fetchPosts})(PostsIndex);
+function mapStateToProps(state){
+    return {posts: state.posts.all};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,34 +1,74 @@
-import React,{Component} from 'react';
+import React,{Component, PropTypes} from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsNew extends Component{
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props){
+        this.props.createPost(props)
+            .then(() => {
+            //blog post has been created, navigate the user to the index
+            // We navigate by calling this.context.router.push with the new path to navigate to.
+            this.context.router.push('/');
+        });
+    }
+
     render(){
         const { fields: {title, categories, content}, handleSubmit} = this.props;
-        console.log(title);
         return (
-            <form onSubmit={handleSubmit(this.props.createPost)}>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <h3>Create a new post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {title.touched ? categories.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <input type="text" className="form-control" {...content} />
+                    <div className="text-help">
+                        {title.touched ? content.error : ''}
+                    </div>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger" >Cancel</Link>
             </form>
         );
     }
+}
+
+function validate(values){
+    const errors = {};
+
+    if(!values.title){
+        errors.title = 'Enter a title';
+    }
+    if(!values.categories){
+        errors.categories = 'Enter a category';
+    }
+    if(!values.content){
+        errors.content = 'Enter content';
+    }
+
+    return errors;
 }
 //connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
 //reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchTopProps
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content']
+    fields: ['title', 'categories', 'content'],
+    validate
 }, null, {createPost})(PostsNew);//similar to connect method
